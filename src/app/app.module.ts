b67f6053ee8ae4b52f46b056a1d2f8bf7376ea43
Anderson Matos <ams.bns2021@gmail.com>
2021-11-25 07:14:40 -0300

    master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routing.module';
+import { ErrorsModule } from './errors/errors.module';
+import { PhotosModule } from './photos/photos.module';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PhotosModule,
+    AppRoutingModule,
+    ErrorsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
